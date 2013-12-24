89453780edfc135ebb9018d8cfd105ab1022b1aa
Jonathan Park <jpark@home.com>
2013-12-24 08:08:05 -0800

    Updates

--- parkbot.ino
+++ parkbot.ino
@@ -2,6 +2,7 @@
 // copyright Adafruit Industries LLC, 2009
 // this code is public domain, enjoy!
 #include <AFMotor.h>
+#include <UFDistance.h>
 
 AF_DCMotor motor1(1);
 
@@ -11,66 +12,66 @@ AF_DCMotor motor3(3);
 
 AF_DCMotor motor4(4);
 
+UFDistance dist;
+
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
   Serial.println("Motor test!");
-
+  dist.init();
   // turn on motor
 }
 
 void loop() {
-  float i;
+  Serial.println("Test distance");
+  dist.record();
   
-  Serial.print("tick");
-  
-  motor1.run(FORWARD);
-  motor2.run(FORWARD);
-  motor3.run(BACKWARD);
-  motor4.run(BACKWARD);
-  for (i=55; i<255; i++) {
-    motor1.setSpeed(i);  
-    motor2.setSpeed(i);  
-    motor3.setSpeed(i);  
-    motor4.setSpeed(i);  
-    delay(10);
- }
- 
-  for (i=255; i!=0; i--) {
-    motor1.setSpeed(i);  
-    motor2.setSpeed(i);  
-    motor3.setSpeed(i);  
-    motor4.setSpeed(i);
-    delay(10);  
- }
-  
-  Serial.print("tock");
-
-  motor1.run(BACKWARD);
-  motor2.run(BACKWARD);
-  motor3.run(FORWARD);
-  motor4.run(FORWARD);
-  for (i=0; i<255; i++) {
-    motor1.setSpeed(i);  
-    motor2.setSpeed(i);  
-    motor3.setSpeed(i);  
-    motor4.setSpeed(i); 
-    delay(10); 
- }
- 
-  for (i=255; i!=0; i--) {
-    motor1.setSpeed(i);  
-    motor2.setSpeed(i);  
-    motor3.setSpeed(i);  
-    motor4.setSpeed(i);  
-    delay(10);
- }
-  
-
-  Serial.print("tech");
-  motor1.run(RELEASE);
-  motor2.run(RELEASE);
-  motor3.run(RELEASE);
-  motor4.run(RELEASE);
-  delay(1000);
+  Serial.println(dist.getLast());
+  if(dist.getLast() > 10)
+  {
+    Serial.println("Go forward");
+    goForward(255);
+  }
+  else {
+    Serial.println("Go left");
+    goLeft(255);
+    Serial.println("Going left");
+  }
+  delay(100);
 }
+
+
+void goForward(int goRate) {
+    motor1.run(FORWARD);
+    motor2.run(FORWARD);
+    motor3.run(FORWARD);
+    motor4.run(FORWARD);
+    motor1.setSpeed(goRate);
+    motor2.setSpeed(goRate);
+    motor3.setSpeed(goRate);
+    motor4.setSpeed(goRate);
+}
+
+void goLeft(int goRate) {
+    motor1.run(BACKWARD);
+    motor2.run(BACKWARD);
+    motor3.run(FORWARD);
+    motor4.run(FORWARD);
+    motor1.setSpeed(goRate);
+    motor2.setSpeed(goRate);
+    motor3.setSpeed(goRate);
+    motor4.setSpeed(goRate);
+}
+void goRight(int goRate) {
+    motor1.run(FORWARD);
+    motor2.run(FORWARD);
+    motor3.run(BACKWARD);
+    motor4.run(BACKWARD);
+    motor1.setSpeed(goRate);
+    motor2.setSpeed(goRate);
+    motor3.setSpeed(goRate);
+    motor4.setSpeed(goRate);
+}
+
+
+
 
